d82dfe001e92466ac27d59a6f4cced9875cfdbc4
MSI\thi ff vlog <thidovan58@gmail.com>
2023-12-20 09:24:38 +0700

    lab4

---
+++ lab4/Excercise1/Information_app/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name, address, phone;
+
+        Console.Write("Enter your name: ");
+        name = Console.ReadLine();
+
+        Console.Write("Enter your address: ");
+        address = Console.ReadLine();
+
+        Console.Write("Enter your phone number: ");
+        phone = Console.ReadLine();
+
+        Console.WriteLine("\nYour Information:");
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Address: " + address);
+        Console.WriteLine("Phone Number: " + phone);
+
+    }
+}
---
+++ lab4/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1, num2, num3;
+
+        Console.Write("Enter the first integer: ");
+        num1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter the second integer: ");
+        num2 = int.Parse(Console.ReadLine());
+        Console.Write("Enter the third integer: ");
+        num3 = int.Parse(Console.ReadLine());
+        int maxNumber = FindMax(num1, num2, num3);
+
+        Console.WriteLine("\nThe maximum number is: " + maxNumber);
+
+    }
+
+    static int FindMax(int a, int b, int c)
+    {
+        int max = a;
+
+        if (b > max)
+            max = b;
+
+        if (c > max)
+            max = c;
+
+        return max;
+    }
+}
